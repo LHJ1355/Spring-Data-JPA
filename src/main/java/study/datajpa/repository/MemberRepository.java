8fa9193f86b8ca46f9f86643957be497499427d0
LHJ1355 <lhj56411355@gmail.com>
2020-09-18 19:57:51 +0900

    벌크성 수정 쿼리

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,12 +1,17 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.domain.Member;
 import study.datajpa.dto.MemberDto;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     /**
@@ -15,7 +20,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsername(String username);
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
     //List<Member> findTop3By();   //Limit, 조회한 것중 3개
-    //Member find___By();   //___에는 메소드에대한 설명
+    //List<Member> find___By();   //___에는 메소드에대한 설명
     //List<Member> findDistinctBy();  //DISTINCT
 
     /**
@@ -35,4 +40,32 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name)from Member m join m.team t")
     List<MemberDto> findMemberDto();
 
+    /**
+     * 반환 타입
+     */
+    List<Member> findListByUsername(String username);   //컬렉션
+    Member findMemberByUsername(String username);   //단건
+    Optional<Member> findOptionalByUsername(String username);   //단건 Optional
+
+    /**
+     * 페이징과 정렬
+     */
+    //Page는 totalCount 필요, 자동으로 count 쿼리가 발생
+    //Page의 필드값 totalPage, totalElement 는 totalCount 값이 있어야 계산 가능
+    Page<Member> findPageByUsername(String username, Pageable pageable);
+
+    //Slice는 totalCount 불필요, count 쿼리 발생 x
+    //Slice는 다음페이지의 유,무를 요청하는 쿼리의 size(limit) 를 하나 더 키워서 가져옴으로써 판단함
+    Slice<Member> findSliceByUsername(String username, Pageable pageable);
+
+    //count query 는 join을 하지 않도록 최적화 할 수 있다.
+    @Query(value = "select m from Member m join m.team", countQuery = "select count(m) from Member m")
+    Page<Member> findByAge(int age, Pageable pageable);
+
+    /**
+     * 벌크성 수정 쿼리
+     */
+    @Modifying(clearAutomatically = true)  //조회쿼리가 아닌 INSERT, UPDATE, DELETE 등, 변경, 삭제 쿼리메소드를 실행할 때 필요
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);     //update return 값 으로는, update 된 row 수가 나옴
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -4,13 +4,19 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.domain.Member;
 import study.datajpa.domain.Team;
 import study.datajpa.dto.MemberDto;
 
+import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 @Rollback(false)
 class MemberRepositoryTest {
+    @Autowired private EntityManager em;
     @Autowired private MemberRepository memberRepository;
     @Autowired private TeamRepository teamRepository;
 
@@ -71,7 +78,6 @@ class MemberRepositoryTest {
 
     @Test
     void findMemberDto(){
-
         //given
         Team team = new Team("teamA");
         teamRepository.save(team);
@@ -88,5 +94,91 @@ class MemberRepositoryTest {
         memberDtos.stream().forEach(m -> {
             System.out.println("memberDto -> " + m);
         });
+    }
+
+    @Test
+    void returnType(){
+        //given
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member memberA = Member.createMember("memberA", 10, team);
+        Member memberA_1 = Member.createMember("memberA", 15, team);
+        Member memberB = Member.createMember("memberB", 20, team);
+
+        memberRepository.save(memberA);
+        memberRepository.save(memberA_1);
+        memberRepository.save(memberB);
+        
+        //when
+        List<Member> memberList = memberRepository.findListByUsername("memberA");
+        Member member = memberRepository.findMemberByUsername("memberB");
+        Optional<Member> optionalMember = memberRepository.findOptionalByUsername("memberB");
+
+        //then
+        memberList.stream().forEach(m -> System.out.println("memberList -> " + m));
+        System.out.println("member -> " + member);
+        System.out.println("optional Member -> " + optionalMember);
+    }
+
+    @Test
+    void paging(){
+        //given
+        Team team = new Team("team");
+        teamRepository.save(team);
+
+        memberRepository.save(Member.createMember("memberA", 10, team));
+        memberRepository.save(Member.createMember("memberA", 20, team));
+        memberRepository.save(Member.createMember("memberA", 30, team));
+        memberRepository.save(Member.createMember("memberA", 40, team));
+        memberRepository.save(Member.createMember("memberA", 50, team));
+
+        //then
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+        Page<Member> pagedMember = memberRepository.findPageByUsername("memberA", pageRequest);
+        Slice<Member> slicedMember = memberRepository.findSliceByUsername("memberA", pageRequest);
+
+        //Page 의 content를 Dto로 맵핑
+        Page<MemberDto> toMap = pagedMember.map(m -> new MemberDto(m.getId(), m.getUsername(), m.getTeam().getName()));
+
+
+        //then
+        List<Member> content = pagedMember.getContent();
+
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(pagedMember.getTotalElements()).isEqualTo(5);
+        assertThat(pagedMember.getNumber()).isEqualTo(0);
+        assertThat(pagedMember.getTotalPages()).isEqualTo(2);
+        assertThat(pagedMember.isFirst()).isTrue();
+        assertThat(pagedMember.hasNext()).isTrue();
+    }
+
+    @Test
+    void bulkAgePlus(){
+        //given
+        Team team = new Team("team");
+        teamRepository.save(team);
+
+        memberRepository.save(Member.createMember("memberA", 10, team));
+        memberRepository.save(Member.createMember("memberA", 20, team));
+        memberRepository.save(Member.createMember("memberA", 30, team));
+        memberRepository.save(Member.createMember("memberA", 40, team));
+        memberRepository.save(Member.createMember("memberA", 50, team));
+
+        //then
+        //영속성 컨텍스트에 저장된 member들을 DB에 직접 쿼리를 날리는 JPQL이 변경할 수 있는 이유
+        //JPQL을 실행하는 경우 영속성 컨텍스트의 쿼리들을 모아놓은 쓰기 지연 SQL 저장소의 쿼리들이 flush되어 DB에 반영되기 때문이다.
+        int cnt = memberRepository.bulkAgePlus(30);
+
+        //벌크 연산은 db에 직접 query 하기 때문에 영속성 컨텍스트 와 동일성이 보장 되지 않는다. db 값은 변경되도 영속성 컨텍스트는 변경 x
+        //따라서 벌크연산 후에는 영속성 컨텍스트를 비워줘야 한다.
+        //em.clear();   //em.clear() 대신 @Modifying 의 옵션을 clearAutomatically = true 로 두어도 된다.
+
+        List<Member> members = memberRepository.findByUsername("memberA");
+        members.stream().forEach(m -> System.out.println("member -> " + m));
+
+        //then
+        assertThat(cnt).isEqualTo(3);
     }
 }
